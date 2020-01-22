375aea6ba693d71388c742837b4d87b5806c39f8
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-22 23:57:48 +0600

    New field added and login page action add for common user registration

--- app/src/main/java/com/eh/paraparbd/CommonUserRegistration.java
+++ app/src/main/java/com/eh/paraparbd/CommonUserRegistration.java
@@ -2,13 +2,46 @@ package com.eh.paraparbd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CommonUserRegistration extends AppCompatActivity {
 
+	Button btnSignUpCU;
+	Button btnLoginPage;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_common_user_registration);
+
+		btnSignUpCU = findViewById(R.id.btnSignUp);
+		btnLoginPage = findViewById(R.id.btnLoginPage);
+
+
+		//After Login page button press
+		btnLoginPage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(CommonUserRegistration.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+		//After sign up button press
+//		btnSignUpCU.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				String phoneNo = btnPhoneNo.getText().toString();
+//				String password = btnPassword.getText().toString();
+
+//				if (phoneNo.equals("01670932273") && password.equals("1234")) {
+//					mySuccessToast();
+//				} else {
+//					myErrorToast();
+//				}
+//			}
+//		});
+
 	}
 }
