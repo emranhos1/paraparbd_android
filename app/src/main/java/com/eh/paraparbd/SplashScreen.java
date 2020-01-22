5237ec13b160c2c54902adac59938acf465c5926
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-22 23:56:47 +0600

    New splash screen added

---
+++ app/src/main/java/com/eh/paraparbd/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.eh.paraparbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		getSupportActionBar().setTitle("");
+        setContentView(R.layout.activity_splash_screen);
+
+		//This handler works for Splash screen.
+		int SPLASH_TIME_OUT = 2000;
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+				startActivity(homeIntent);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+    }
+}
