81d7a35eb919cd93d842ec3390c853fc37129724
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-24 21:23:41 +0600

    Add splash screen

--- app/src/main/java/com/eh/paraparbd/SplashScreen.java
+++ app/src/main/java/com/eh/paraparbd/SplashScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.eh.paraparbd.login.MainActivity;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
