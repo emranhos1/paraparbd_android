7bff501af516b43dd6de838911f0ed67bd95ba1d
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-24 21:26:41 +0600

    Create new activity for RentACar Booking

---
+++ app/src/main/java/com/eh/paraparbd/commonuser/RentACarBooking.java
@@ -0,0 +1,16 @@
+package com.eh.paraparbd.commonuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eh.paraparbd.R;
+
+public class RentACarBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_acar_booking);
+    }
+}
