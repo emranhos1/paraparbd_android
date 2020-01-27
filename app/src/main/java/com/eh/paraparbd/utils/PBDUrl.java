0bad20e16dadbcb2b3ba2b3f47a4c71cba097289
Md Emran Hossain <emran@medisys.com.sa>
2020-01-27 10:23:54 +0000

    Change URL

--- app/src/main/java/com/eh/paraparbd/utils/PBDUrl.java
+++ app/src/main/java/com/eh/paraparbd/utils/PBDUrl.java
@@ -2,6 +2,6 @@ package com.eh.paraparbd.utils;
 
 public class PBDUrl {
 
-//    public final static String BASE_URL = "http://100.43.0.40:9191";//Medisys
-    public final static String BASE_URL = "http://192.168.0.107:9191";//Home
+    public final static String BASE_URL = "http://100.43.0.40:9191";//Medisys
+//    public final static String BASE_URL = "http://192.168.0.107:9191";//Home
 }
