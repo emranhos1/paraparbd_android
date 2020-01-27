f31b348c09d37040e1ef4088db1c053ce6fe0ac3
Md Emran Hossain <emran@medisys.com.sa>
2020-01-27 15:11:16 +0600

    Move GetLoginInfo method in SignInfo class

--- app/src/main/java/com/eh/paraparbd/login/MainActivity.java
+++ app/src/main/java/com/eh/paraparbd/login/MainActivity.java
@@ -11,49 +11,32 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.eh.paraparbd.R;
-import com.eh.paraparbd.Service.PBDApi;
-import com.eh.paraparbd.commonuser.CommonUserDashboard;
-import com.eh.paraparbd.model.LoginTable;
-import com.eh.paraparbd.pojo.LoginCollection;
+import com.eh.paraparbd.classes.SignInfo;
 import com.eh.paraparbd.utils.AlartUtil;
 import com.eh.paraparbd.utils.PBDUtil;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
 	Context context;
-
 	EditText btnPhoneNo, btnPassword;
 	Button btnLogin, btnSignUp;
 	String phoneNo, password;
-
 	final static String TAG = "MainActivity";
-
-	private static LoginTable loginTable = new LoginTable();
-	private static PBDApi pbdApi = PBDUtil.webserviceInitialize();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		context = MainActivity.this;
 		super.onCreate(savedInstanceState);
 
-		//Set action bar
-		//getSupportActionBar().setLogo(R.drawable.parapar);
-		//getSupportActionBar().setDisplayShowTitleEnabled(false);
-		getSupportActionBar().setDisplayShowHomeEnabled(true);
-		getSupportActionBar().setDisplayUseLogoEnabled(true);
 		getSupportActionBar().setTitle("");
 		setContentView(R.layout.activity_main);
 
+		context = MainActivity.this;
 		btnPhoneNo = findViewById(R.id.phone_no);
 		btnPassword = findViewById(R.id.password);
 		btnLogin = findViewById(R.id.btnLogin);
 		btnSignUp = findViewById(R.id.btnSignUp);
 
-		//After sign up button press
+		//Sign up button Action
 		btnSignUp.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -62,97 +45,31 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 
+		//Login Button Action
 		btnLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				phoneNo = btnPhoneNo.getText().toString();
 				password = btnPassword.getText().toString();
+				Log.d(TAG, "Given Phone No"+ phoneNo);
+				Log.d(TAG, "Given Password"+ password);
 
-				Log.d(TAG, phoneNo);
-				Log.d(TAG, password);
-
-				if(!phoneNo.isEmpty()){
-					if(!password.isEmpty()){
-
-//						AlartUtil.showAlartDialog(context, "Success", "Login Successful!!", true);
-//						Toast.makeText(context, "Phone No :"+phoneNo, Toast.LENGTH_SHORT).show();
-//						Toast.makeText(context, "Password :"+password, Toast.LENGTH_SHORT).show();
-//						if (phoneNo.equals("01670932273") && password.equals("1234")) {
-
-//							Intent intent = new Intent(MainActivity.this, CommonUserDashboard.class);
-//							startActivity(intent);
-//							finish();
-
-//						} else {
-//							Toast.makeText(context, "Not Match", Toast.LENGTH_SHORT).show();
-//						}
-						if(PBDUtil.isInternetConnected(context) == true){
-							getLoginInfo(context, phoneNo, password);
-
+				if (!phoneNo.isEmpty()) {
+					if (!password.isEmpty()) {
+						if (PBDUtil.isInternetConnected(context)) {
+							SignInfo.getLoginInfo(context, phoneNo, password);
 						} else {
 							String title = "No Internet Connection";
 							String message = "Please check your internet connection";
 							boolean status = false;
-//							AlartUtil.showAlartDialog(context, title, message, status);
+							AlartUtil.showAlartDialog(context, title, message, status);
 						}
-					} else{
+					} else {
 						btnPassword.setError("Password Should Not Be Blank");
 					}
 				} else {
 					btnPhoneNo.setError("UserId Should Not Be Blank");
 				}
-			}
-		});
-
-	}
-
-	public void getLoginInfo (final Context context, String phoneNo, String password){
-//		AlartUtil.showProgressDialog(context);
-		Log.d(TAG, "user phone and pass :: " + phoneNo+" "+password);
-		loginTable.setPhoneNo(phoneNo);
-		loginTable.setPassword(password);
-
-		Call<LoginCollection> getInfo = pbdApi.userLogin(loginTable);
-		getInfo.enqueue(new Callback<LoginCollection>() {
-			@Override
-			public void onResponse(Call<LoginCollection> call, Response<LoginCollection> response) {
-
-				try {
-					LoginCollection responseData =  response.body();
-					Log.d(TAG, "DATA :: " + responseData.getSuccess());
-					if(responseData.getSuccess()){
-						LoginTable login = responseData.getData();
-						Log.d(TAG, "AllUser Id :: " + login.getAllUserId());
-						Log.d(TAG, "Active status :: " + login.getActiveStatus());
-						Log.d(TAG, "Role :: " + login.getUserRole());
-//						//now go to welcome activity
-//						Intent welcomeIntent = new Intent(context, CommonUserDashboard.class);
-//						//welcomeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//						welcomeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//						welcomeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//						welcomeIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-//						startActivity(welcomeIntent);
-
-						Intent intent = new Intent(context, CommonUserDashboard.class);
-						startActivity(intent);
-						finish();
-						Log.d(TAG,"Login success!!!##############################");
-					}
-//					AlartUtil.hideProgressDialog();
-				} catch (Exception e){
-					Log.d(TAG,"list is null");
-					e.printStackTrace();
-//					AlartUtil.hideProgressDialog();
-//					AlartUtil.showAPInotResponseWarn(context);
-				}
-			}
-
-			@Override
-			public void onFailure(Call<LoginCollection> call, Throwable t) {
-				Log.d(TAG,t.getMessage());
-				//Hide Dialog
-//				AlartUtil.hideProgressDialog();
-//				AlartUtil.showAPInotResponseWarn(context);
 			}
 		});
 	}
