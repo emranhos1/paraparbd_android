c86827de0c6edfd3cabfb7da0c25ec842a711def
Md Emran Hossain <emran@medisys.com.sa>
2020-01-27 17:49:35 +0600

    Create new class for Common User Registration

---
+++ app/src/main/java/com/eh/paraparbd/classes/Registration.java
@@ -0,0 +1,14 @@
+package com.eh.paraparbd.classes;
+
+import android.content.Context;
+
+import com.eh.paraparbd.model.CommonUserRegTable;
+import com.eh.paraparbd.utils.AlertUtil;
+
+public class Registration {
+	public static void commonUserRegistration(Context context, CommonUserRegTable commonUserRegTable) {
+		AlertUtil.showProgressDialog(context);
+		//TODO
+		//FIXME
+	}
+}
