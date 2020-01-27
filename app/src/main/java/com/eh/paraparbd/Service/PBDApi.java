4a80c3a4f846b05bad8850ae222fc38184395ae9
Md Emran Hossain <emran@medisys.com.sa>
2020-01-27 18:06:16 +0600

    Declar a new method for  common user Registration

--- app/src/main/java/com/eh/paraparbd/Service/PBDApi.java
+++ app/src/main/java/com/eh/paraparbd/Service/PBDApi.java
@@ -1,7 +1,8 @@
 package com.eh.paraparbd.Service;
 
+import com.eh.paraparbd.model.CommonUserRegTable;
 import com.eh.paraparbd.model.LoginTable;
-import com.eh.paraparbd.pojo.LoginCollection;
+import com.eh.paraparbd.pojo.JsonCollection;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -10,5 +11,8 @@ import retrofit2.http.POST;
 public interface PBDApi {
 
     @POST("/login")
-    Call<LoginCollection> userLogin(@Body LoginTable loginTable);
+    Call<JsonCollection> userLogin(@Body LoginTable loginTable);
+
+    @POST("/common-user/registration")
+    Call<JsonCollection> commonUserRegistration(CommonUserRegTable commonUserRegTable);
 }
