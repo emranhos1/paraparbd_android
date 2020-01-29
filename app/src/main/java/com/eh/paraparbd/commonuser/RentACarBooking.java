4a0ed9dc0745a0ddd0c19c97cabfc6c7c700f2f9
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-29 19:40:45 +0600

    Modify code for print values

--- app/src/main/java/com/eh/paraparbd/commonuser/RentACarBooking.java
+++ app/src/main/java/com/eh/paraparbd/commonuser/RentACarBooking.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -25,9 +26,10 @@ public class RentACarBooking extends AppCompatActivity implements DatePickerDial
     Button btnDatePicker,btnSendRequest;
 //    Button btnTimePicker;
     TextView dateText, timeText;
+    EditText addressPickText, addressDropText, totalAmountText;
+    String addressPickup, addressDrop, totalAmount, divisionName, divisionDropName;
+    String districtName, districtDropName, policeStationName, policeStationDropName, carName;
     Context context;
-
-    final static String TAG = "RentACarBooking";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +39,35 @@ public class RentACarBooking extends AppCompatActivity implements DatePickerDial
         getSupportActionBar().setTitle("Rent A Car Booking");
         setContentView(R.layout.activity_rent_acar_booking);
 
-        Spinner divisionSpinner = findViewById(R.id.spinner_division);
-        Spinner districtSpinner = findViewById(R.id.spinner_district);
-        Spinner policeStationSpinner = findViewById(R.id.spinner_police_station);
+        final Spinner divisionSpinner = findViewById(R.id.spinner_division);
+        final Spinner divisionDropSpinner = findViewById(R.id.spinner_drop_division);
+        final Spinner districtSpinner = findViewById(R.id.spinner_district);
+        final Spinner districtDropSpinner = findViewById(R.id.spinner_drop_district);
+        final Spinner policeStationSpinner = findViewById(R.id.spinner_police_station);
+        final Spinner policeStationDropSpinner = findViewById(R.id.spinner_drop_police_station);
+        final Spinner carSpinner = findViewById(R.id.spinner_car);
+        addressPickText = findViewById(R.id.pickup_address);
+        addressDropText = findViewById(R.id.drop_address);
+        totalAmountText = findViewById(R.id.total_amount);
 
         //division
         ArrayAdapter<CharSequence> divisionAdapter = CustomSpinnerArrayAdapter.getAdapter(context, R.array.division);
         CustomSpinnerArrayAdapter.setSpinner(this, divisionSpinner, divisionAdapter);
+        CustomSpinnerArrayAdapter.setSpinner(this, divisionDropSpinner, divisionAdapter);
 
         //district
         ArrayAdapter<CharSequence> districtAdapter = CustomSpinnerArrayAdapter.getAdapter(context, R.array.district);
         CustomSpinnerArrayAdapter.setSpinner(this, districtSpinner, districtAdapter);
+        CustomSpinnerArrayAdapter.setSpinner(this, districtDropSpinner, districtAdapter);
 
         //police station
         ArrayAdapter<CharSequence> policeStationAdapter = CustomSpinnerArrayAdapter.getAdapter(context, R.array.policeStation);
         CustomSpinnerArrayAdapter.setSpinner(this, policeStationSpinner, policeStationAdapter);
+        CustomSpinnerArrayAdapter.setSpinner(this, policeStationDropSpinner, policeStationAdapter);
+
+        //car
+        ArrayAdapter<CharSequence> carAdapter = CustomSpinnerArrayAdapter.getAdapter(context, R.array.car);
+        CustomSpinnerArrayAdapter.setSpinner(this, carSpinner, carAdapter);
 
         btnDatePicker = findViewById(R.id.date_picker);
         btnSendRequest = findViewById(R.id.btn_send_request);
@@ -64,6 +80,27 @@ public class RentACarBooking extends AppCompatActivity implements DatePickerDial
             @Override
             public void onClick(View v) {
                 showDatePickerDailog();
+            }
+        });
+
+        btnSendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                divisionName = divisionSpinner.getSelectedItem().toString();
+                divisionDropName = divisionDropSpinner.getSelectedItem().toString();
+                districtName = districtSpinner.getSelectedItem().toString();
+                districtDropName = districtDropSpinner.getSelectedItem().toString();
+                policeStationName = policeStationSpinner.getSelectedItem().toString();
+                policeStationDropName = policeStationDropSpinner.getSelectedItem().toString();
+                carName = carSpinner.getSelectedItem().toString();
+                addressPickup = addressPickText.getText().toString();
+                addressDrop = addressDropText.getText().toString();
+                totalAmount = totalAmountText.getText().toString();
+                String message = ""+divisionName+" "+divisionDropName+" "+districtName
+                        +" "+districtDropName+" "+policeStationName+" "+policeStationDropName+" "+carName
+                        +" "+addressPickup+" "+addressDrop+" "+totalAmount;
+
+                CustomAlertMessage.showCustomAlert(context, "Division", message, true);
             }
         });
         //btnTimePicker.setOnClickListener(new View.OnClickListener() {
