fed572a5bc87d197b5c012e69950037e8b06b476
Md. Emran Hossain <emranhos1@gmail.com>
2020-01-30 21:50:59 +0600

    Create new model for division

---
+++ app/src/main/java/com/eh/paraparbd/model/DivisionTable.java
@@ -0,0 +1,34 @@
+package com.eh.paraparbd.model;
+
+import java.util.Date;
+
+public class DivisionTable {
+
+    private int divisionId;
+    private String divisionName;
+    private Date createdDate;
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
