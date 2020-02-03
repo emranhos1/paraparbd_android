4ec46d2db8b1ad7be30b7a177dbdd1e70e741890
Md. Emran Hossain <emranhos1@gmail.com>
2020-02-03 22:42:28 +0600

    Modify PBDApi code for get data

--- app/src/main/java/com/eh/paraparbd/Service/PBDApi.java
+++ app/src/main/java/com/eh/paraparbd/Service/PBDApi.java
@@ -2,9 +2,12 @@ package com.eh.paraparbd.Service;
 
 import com.eh.paraparbd.model.CommonUserRegTable;
 import com.eh.paraparbd.model.LoginTable;
+import com.eh.paraparbd.pojo.CarBrandCollection;
 import com.eh.paraparbd.pojo.CommonUserRegCollection;
+import com.eh.paraparbd.pojo.DistrictCollection;
 import com.eh.paraparbd.pojo.DivisionCollection;
 import com.eh.paraparbd.pojo.LoginCollection;
+import com.eh.paraparbd.pojo.PoliceStationCollection;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -21,4 +24,13 @@ public interface PBDApi {
 
     @GET("/place/division")
     Call<DivisionCollection> getAllDivision();
+
+    @GET("/place/district")
+    Call<DistrictCollection> getAllDistrict();
+
+    @GET("/place/police-station")
+    Call<PoliceStationCollection> getAllPoliceStation();
+
+    @GET("/car/brand")
+    Call<CarBrandCollection> getAllCarBrand();
 }
