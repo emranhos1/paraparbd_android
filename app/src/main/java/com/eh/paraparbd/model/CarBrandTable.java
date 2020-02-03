525d732bdfb3145e42c9e688fba7bbb85dddaffb
Md. Emran Hossain <emranhos1@gmail.com>
2020-02-03 22:38:10 +0600

    Create new model for Car Brand

---
+++ app/src/main/java/com/eh/paraparbd/model/CarBrandTable.java
@@ -0,0 +1,34 @@
+package com.eh.paraparbd.model;
+
+import java.util.Date;
+
+public class CarBrandTable {
+
+    private int carBrandId;
+    private String brandName;
+    private Date createdDate;
+
+    public int getCarBrandId() {
+        return carBrandId;
+    }
+
+    public void setCarBrandId(int carBrandId) {
+        this.carBrandId = carBrandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
