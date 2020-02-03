b15ebaad0cad29ee2ef25f5b9cbbd9dc8e76218d
Md. Emran Hossain <emranhos1@gmail.com>
2020-02-03 22:38:31 +0600

    Create new model for District

---
+++ app/src/main/java/com/eh/paraparbd/model/DistrictTable.java
@@ -0,0 +1,43 @@
+package com.eh.paraparbd.model;
+
+import java.util.Date;
+
+public class DistrictTable {
+
+    private int districtId;
+    private int divisionId;
+    private String districtName;
+    private Date createdDate;
+
+    public int getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(int districtId) {
+        this.districtId = districtId;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
